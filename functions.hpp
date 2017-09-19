c0c40f4637a5035297210aae7bd3f4f9a1dc71f6
Roberto Ostinelli <roberto@ostinelli.net>
2017-09-19 09:40:15 +0200

    Extract functions and sounds in own files.

---
+++ functions.hpp
@@ -0,0 +1,52 @@
+class CfgFunctions {
+
+    class SCAR_UCM {
+        tag = "SCAR_UCM";
+
+        class functions {
+            file = "\scar_ucm\functions";
+
+            // module
+            class moduleUtilitiesConstructionMod {};
+
+            // inits
+            class initServer {};
+            class initPlayer {};
+            class initBoss {};
+            class initSettings {};
+
+            // events
+            class onUnloadedCargoPos {};
+
+            // loops
+            class loopWorkerMovements {};
+            class loopConstructionProgress {};
+
+            // actions
+            class addActionsToBoss {};
+            class addActionsToWorker {};
+            class addActionRequestStatus {};
+            class addActionWorkerGetIn {};
+            class addActionWorkerGetOut {};
+
+            // resources
+            class requestWorkers {};
+            class requestMaterial {};
+            class createWorkers {};
+            class dropMaterialFromHelicopter {};
+
+            // getters & setters
+            class getCurrentPiece {};
+            class getPiecesCount {};
+            class setAltitudeToGround {};
+            class setWorkerAnimation {};
+            class setMarkerConstruction {};
+            class setMarkerWorker {};
+            class setGlobalVariableIfUnset {};
+            class setRandomWorkerLoadout {};
+
+            // other
+            class safetyDeleteVehicleAndCrew {};
+        };
+    };
+};
---
+++ sounds.hpp
@@ -0,0 +1,52 @@
+class CfgSounds
+{
+    sounds[] = {};
+    class SCAR_UCM_working_1
+    {
+        name = "working_1";
+        sound[] = {"\scar_ucm\sounds\chisel-45.ogg", 1, 1, 150}; // filename, volume, pitch, distance
+        titles[] = {};
+    };
+    class SCAR_UCM_working_2
+    {
+        name = "working_2";
+        sound[] = {"\scar_ucm\sounds\grinder_small-28.ogg", 1, 1, 150};
+        titles[] = {};
+    };
+    class SCAR_UCM_working_3
+    {
+        name = "working_3";
+        sound[] = {"\scar_ucm\sounds\grinder-62.ogg", 1, 1, 150};
+        titles[] = {};
+    };
+    class SCAR_UCM_working_4
+    {
+        name = "working_4";
+        sound[] = {"\scar_ucm\sounds\hammering-6.ogg", 1, 1, 150};
+        titles[] = {};
+    };
+    class SCAR_UCM_working_5
+    {
+        name = "working_5";
+        sound[] = {"\scar_ucm\sounds\sander-31.ogg", 1, 1, 150};
+        titles[] = {};
+    };
+    class SCAR_UCM_working_6
+    {
+        name = "working_6";
+        sound[] = {"\scar_ucm\sounds\scratching-16.ogg", 1, 1, 150};
+        titles[] = {};
+    };
+    class SCAR_UCM_working_7
+    {
+        name = "working_7";
+        sound[] = {"\scar_ucm\sounds\wrench_losen-10.ogg", 1, 1, 150};
+        titles[] = {};
+    };
+    class SCAR_UCM_working_8
+    {
+        name = "working_8";
+        sound[] = {"\scar_ucm\sounds\wrench-10.ogg", 1, 1, 150};
+        titles[] = {};
+    };
+};
